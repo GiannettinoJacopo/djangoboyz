c945256bd2a6420f4cffd571d5132591ed84dc3d
Domenico Cosentino <d.cosentino@4sigma.it>
2022-06-14 10:55:16 +0200

    ho cambiato l'HTML per questo sito

--- manage.py
+++ manage.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
         # exceptions on Python 2.
         try:
             import django
-        except ImportError:
+        except ImportError :
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
-            )
-        raise
+            ) 
     execute_from_command_line(sys.argv)
+
